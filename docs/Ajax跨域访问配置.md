851ccea8b898e31f3725944798442e460e991bbc
SQ_BXZ <1016862070@qq.com>
2019-01-23 14:00:17 +0800

    配置跨域访问

---
+++ docs/Ajax跨域访问配置.md
@@ -0,0 +1,22 @@
+# 配置跨域访问
+```java
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter{
+
+    // 。。。。 
+
+    /**
+     * 跨域请求配置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/*") // 配置可以被跨域的路径，可以任意配置，可以具体到直接请求路径。
+                .allowedMethods("*") // 允许所有的请求方法访问该跨域资源服务器，如：POST、GET、
+                .allowedOrigins("*") // 允许所有的请求域名访问我们的跨域资源，可以固定单条或者多条内容，如："http://www.baidu.com"，只有百度可以访问我们的跨域资源。
+                .allowedHeaders("*"); // 允许所有的请求header访问，可以自定义设置任意请求头信息，如："X-YAUTH-TOKEN"
+    }
+
+}
+```
--- src/main/java/com/buxz/config/WebConfiguration.java
+++ src/main/java/com/buxz/config/WebConfiguration.java
@@ -8,10 +8,7 @@ import com.buxz.interceptor.LoggerInterceptor;
 import com.buxz.interceptor.SessionInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -87,6 +84,19 @@ public class WebConfiguration extends WebMvcConfigurerAdapter
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/");
+    }
+
+    /**
+     * 跨域请求配置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/*") // 配置可以被跨域的路径，可以任意配置，可以具体到直接请求路径。
+                .allowedMethods("*") // 允许所有的请求方法访问该跨域资源服务器，如：POST、GET、
+                .allowedOrigins("*") // 允许所有的请求域名访问我们的跨域资源，可以固定单条或者多条内容，如："http://www.baidu.com"，只有百度可以访问我们的跨域资源。
+                .allowedHeaders("*"); // 允许所有的请求header访问，可以自定义设置任意请求头信息，如："X-YAUTH-TOKEN"
     }
 
 }
